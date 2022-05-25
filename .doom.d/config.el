f0bb11b411347fb497172ede4ffdb7e9b9155a95
John Oerter <john.oerter@passportinc.com>
2022-05-25 08:17:41 -0400

    Changes for dank mono

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -21,7 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Dank Mono" :size 16 :weight 'regular)
+(setq doom-font (font-spec :family "Dank Mono" :size 15 :weight 'regular)
       doom-variable-pitch-font (font-spec :family "Lato" :size 14))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
