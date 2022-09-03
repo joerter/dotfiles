d8fbac5df8e6609994dd46d6d3da769804c088a9
John Oerter <joerter@dinacare.com>
2022-09-03 11:08:33 -0400

    ignore dinacare specific files

--- .oh-my-zsh/custom/dinacare.zsh
+++
@@ -1,7 +0,0 @@
-alias start-entouch-core="cd ~/github/dinacare/entouch-core && nvm use && cd packages/entouch-rest && yarn start:local"
-alias start-enTouch-web="cd ~/github/dinacare/enTouch-web && nvm use && npm run start:local"
-alias start-hub="cd ~/github/dinacare/hub && nvm use && npm run start-web-local"
-alias start-enTouch-mobile="cd ~/github/dinacare/enTouch-mobile && nvm use && gulp clean && gulp serve --env=master"
-
-export FIREBASE_CONFIG=$(cat ~/github/dinacare/entouch-core/packages/entouch-legacy-services/src/gcp/enTouch-master.json)
-
