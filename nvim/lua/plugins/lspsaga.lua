4720eb7f3799f524a6facf72102b85cfb3484887
John Oerter <joerter@dinacare.com>
2023-08-21 10:38:39 -0400

    Lspsaga

--- nvim/lua/plugins/lspsaga.lua
+++ nvim/lua/plugins/lspsaga.lua
@@ -9,7 +9,7 @@ require("lspsaga").setup {
 
 map('n', '<leader>ca', ':Lspsaga code_action<CR>', options)
 map('v', '<leader>ca', ':<C-U>Lspsaga range_code_action<CR>', options)
-map('n', '<leader>cf', ':Lspsaga lsp_finder<CR>', options)
+map('n', '<leader>cf', ':Lspsaga finder<CR>', options)
 map('n', '<leader>ch', ':Lspsaga hover_doc<CR>', options)
 map('n', '<leader>cl', ':Lspsaga show_line_diagnostics<CR>', options)
 map('n', '<leader>cb', ':Lspsaga show_buf_diagnostics<CR>', options)
