4fdb33f798f3233d4cf4c1e35fd024d33710c8fa
John Oerter <joerter@dinacare.com>
2024-01-09 09:48:44 -0500

    Add lspsaga

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -58,6 +58,7 @@ require("lazy").setup({
 				'neovim/nvim-lspconfig',
 				'hrsh7th/cmp-nvim-lsp',
 				"hrsh7th/nvim-cmp",
+				'nvimdev/lspsaga.nvim',
 			}
 		},
 	})
--- nvim/lua/plugins/lspsaga.lua
+++ nvim/lua/plugins/lspsaga.lua
@@ -2,9 +2,6 @@ local map = vim.api.nvim_set_keymap
 options = { noremap = true }
 
 require("lspsaga").setup {
-    ui = {
-        kind = require("catppuccin.groups.integrations.lsp_saga").custom_kind(),
-    },
     lightbulb = {
         enable = false,
     },
@@ -12,7 +9,6 @@ require("lspsaga").setup {
 
 map('n', '<leader>ca', ':Lspsaga code_action<CR>', options)
 map('v', '<leader>ca', ':<C-U>Lspsaga range_code_action<CR>', options)
-map('n', '<leader>cf', ':Lspsaga finder<CR>', options)
 map('n', '<leader>ch', ':Lspsaga hover_doc<CR>', options)
 map('n', '<leader>cl', ':Lspsaga show_line_diagnostics<CR>', options)
 map('n', '<leader>cb', ':Lspsaga show_buf_diagnostics<CR>', options)
