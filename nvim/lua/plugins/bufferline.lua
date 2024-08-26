af46db29b87bc41b85b9dba097cf25d885ec743f
John Oerter <joerter@dinacare.com>
2024-08-25 21:24:44 -0400

    Fix bufferline

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -4,6 +4,7 @@ return {
 	dependencies = "nvim-tree/nvim-web-devicons",
 	opts = {},
 	config = function()
+    require('bufferline').setup({})
 		local map = vim.api.nvim_set_keymap
 		options = { noremap = true }
 
