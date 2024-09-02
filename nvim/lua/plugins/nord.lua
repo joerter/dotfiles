b62e2a7f35087c4141bd728459f177613b6ccfa8
John Oerter <joerter@dinacare.com>
2024-09-02 19:04:21 -0400

    Colorscheme

--- nvim/lua/plugins/nord.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"nordtheme/vim",
-	lazy = false,
-	priority = 1000, -- make sure to load this before all the other start plugins
-	config = function()
-		-- load the colorscheme here
-		vim.cmd([[colorscheme nord]])
-	end,
-}
