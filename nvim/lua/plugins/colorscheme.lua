fa590f8c77a32c7c1fe4e731f167b1f81a095efb
John Oerter <joerter@dinacare.com>
2024-10-16 17:27:36 -0400

    tokyonight

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,22 @@
+-- return {
+--   "nordtheme/vim",
+--   lazy = false,
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--   config = function()
+--     -- load the colorscheme here
+--     vim.cmd([[colorscheme nord]])
+--   end,
+-- }
+
 return {
-  "nordtheme/vim",
+  "folke/tokyonight.nvim",
   lazy = false,
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    -- load the colorscheme here
-    vim.cmd([[colorscheme nord]])
-  end,
+  priority = 1000,
+  opts = {},
+  config = function() vim.cmd [[colorscheme tokyonight-moon]] end,
+  on_highlights = function(highlights, colors)
+    highlights.LineNr = { fg = colors.orange }
+  end
 }
 
 -- return {
