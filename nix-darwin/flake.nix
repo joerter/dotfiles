cb665eb2352bca9e53a0aabd6079608d0acee7ae
John Oerter <joerter@dinacare.com>
2025-01-02 06:04:09 -0500

    Add nix-darwin

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -1,43 +1,153 @@
 {
-  description = "Example nix-darwin system flake";
+  description = "John Oerter's nix-darwin system flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
+    let
+    configuration = { pkgs, config, ... }: {
+
+      nixpkgs.config.allowUnfree = true;
+
+# List packages installed in system profile. To search by name, run:
+# $ nix-env -qaP | grep wget
+      environment.systemPackages = 
+        [
+          #pkgs._1password-gui
+          pkgs.arc-browser
+          pkgs.direnv
+          pkgs.docker
+          pkgs.fzf
+          pkgs.git 
+          pkgs.kitty 
+          pkgs.lazydocker
+          pkgs.lazygit
+          pkgs.mkalias
+          pkgs.mkcert
+          pkgs.neovim
+          pkgs.nodejs_23
+          pkgs.obsidian
+          pkgs.raycast
+          pkgs.ripgrep
+          pkgs.rectangle
+          pkgs.terraform
+          pkgs.tmux
+          pkgs.xz
         ];
 
-      # Necessary for using flakes on this system.
+      homebrew = {
+        enable = true;
+        brews = [
+          "mas"
+        ];
+        casks = [
+        ];
+        # masApps = {
+        #     "Yoink" = ""
+        #   }
+        onActivation.cleanup = "zap";
+        onActivation.autoUpdate = true;
+        onActivation.upgrade = true;
+      };
+
+      # fonts.packages = [
+      #   (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+      # ];
+      # fonts.packages = [
+      #      ...
+      #      pkgs.nerd-fonts._0xproto
+      #      pkgs.nerd-fonts.droid-sans-mono
+      # ]
+
+      # fonts.packages = builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+
+
+# Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+# Enable alternative shell support in nix-darwin.
+# programs.fish.enable = true;
 
-      # Set Git commit hash for darwin-version.
+# Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+# Set up applications.
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read -r src; do
+          app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+            done
+            '';
+
+      system.defaults = {
+         ".GlobalPreferences"."com.apple.mouse.scaling" = 9.0;
+         NSGlobalDomain.AppleInterfaceStyle = "Dark";
+         NSGlobalDomain.KeyRepeat = 7;
+         controlcenter.BatteryShowPercentage = true;
+         controlcenter.Bluetooth = true;
+         controlcenter.Sound = true;
+         dock.autohide = true;
+         dock.appswitcher-all-displays = true;
+         dock.launchanim = false;
+         dock.persistent-others = ["~/github/joerter" "~/Downloads" "~/Screenshots"];
+         dock.showhidden = true;
+         dock.static-only = true;
+         finder.AppleShowAllFiles = true;
+         finder.CreateDesktop = false;
+         finder.ShowPathbar = true;
+         finder.ShowStatusBar = true;
+         hitoolbox.AppleFnUsageType = "Show Emoji & Symbols";
+         loginwindow.GuestEnabled = false;
+         menuExtraClock.ShowSeconds = true;
+         universalaccess.reduceMotion = true;
+         
+
+         finder.FXPreferredViewStyle = "clmv";
+        };
+
+        system.keyboard.remapCapsLockToControl = true;
+
+# Used for backwards compatibility, please read the changelog before changing.
+# $ darwin-rebuild changelog
       system.stateVersion = 5;
 
-      # The platform the configuration will be used on.
+# The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
     };
   in
   {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#maverick
+# Build darwin flake using:
+# $ darwin-rebuild build --flake .#maverick
     darwinConfigurations."maverick" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            enableRosetta = true;
+            user = "joerter";
+          };
+        }];
     };
+
+    darwinPackages = self.darwinConfigurations."twilight".pkgs;
   };
 }
