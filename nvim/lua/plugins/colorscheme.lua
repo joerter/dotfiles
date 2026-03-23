312341b49837d5ee0c3a2266c333203fad7a1726
John Oerter <john@johnoerter.me>
2026-03-23 08:35:10 -0400

    Add aerospace

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,3 @@
--- return {
---   "nordtheme/vim",
---   lazy = false,
---   priority = 1000, -- make sure to load this before all the other start plugins
---   config = function()
---     -- load the colorscheme here
---     vim.cmd([[colorscheme nord]])
---   end,
--- }
 return {
   "folke/tokyonight.nvim",
   lazy = false,
@@ -14,9 +5,6 @@ return {
   config = function()
     require("tokyonight").setup({
       style = "moon",
-      -- on_colors = function(colors)
-      --   colors.fg_gutter = "#ff9e64"
-      -- end
       on_highlights = function(highlights, colors)
         highlights.LineNr = { fg = colors.magenta }
         highlights.LineNrAbove = { fg = colors.magenta }
@@ -28,13 +16,3 @@ return {
     vim.cmd [[colorscheme tokyonight-moon]]
   end
 }
-
--- return {
---   "craftzdog/solarized-osaka.nvim",
---   lazy = false,
---   priority = 1000,
---   opts = {},
---   config = function ()
---     vim.cmd[[colorscheme solarized-osaka]]
---   end,
--- }
